0fba2ec8e76a66af9deeb9eb54fd9ebe85841649
kelvin-kariuki <Kariuki405@gmail.com>
2022-06-19 13:10:00 +0300

    Done.

--- index.js
+++ index.js
@@ -1,13 +1,46 @@
 function isPalindrome(word) {
   // Write your algorithm here
+  let array = word.split("")
+  let reversedArray = array.reverse()
+  let reversedWord = reversedArray.join("")
+  if (word == reversedWord){
+    return true;
+  } else {
+    return false;
+  }
 }
+isPalindrome('abba')
+isPalindrome('racecar')
+//  return word.split("").reverse().join("");
+
+// } 
+
+// function isPalindrome(word) {
+//   const reversed = reverseString(word);
+//   return word === reversed;
+// }
+
+
 
 /* 
   Add your pseudocode here
+reverse the input string
+
+if the reversed string is the same as the input
+  return true
+else
+  return false
+// function isPalindrome(word) {
+//   const reversed = reverseString(word);
+//   return word === reversed;
+// }
+  
 */
 
 /*
   Add written explanation of your solution here
+Takes the param and checks whether its parindrome in both ways.
+returns false if otherwise
 */
 
 // You can run `node index.js` to view these console logs
